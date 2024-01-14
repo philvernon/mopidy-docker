5515e2bab3710f051735a0db6526f779ff04b752
philvernon <phil.vernon@synapt.ec>
2024-01-14 19:48:51 +0000

    create pulseaudio socket in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,7 @@
 if [ -z "$PULSE_COOKIE_DATA" ]
 then
     echo -ne $(echo $PULSE_COOKIE_DATA | sed -e 's/../\\x&/g') >$HOME/pulse.cookie
+    pactl load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket
     export PULSE_COOKIE=$HOME/pulse.cookie
 fi
 
